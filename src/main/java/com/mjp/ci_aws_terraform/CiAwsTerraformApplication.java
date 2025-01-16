8cc4b61e75fa37ba16659927f96af3561ecf3e6b
Maycon J Pereira <mayconjovan@gmail.com>
2025-01-16 17:36:46 -0300

    Project created

---
+++ src/main/java/com/mjp/ci_aws_terraform/CiAwsTerraformApplication.java
@@ -0,0 +1,13 @@
+package com.mjp.ci_aws_terraform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiAwsTerraformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiAwsTerraformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjp/ci_aws_terraform/CiAwsTerraformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjp.ci_aws_terraform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiAwsTerraformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
